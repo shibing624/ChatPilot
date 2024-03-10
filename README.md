120e732b04d9903b48c0ac49210ff3e231a340e2
shibing624 <shibing624@126.com>
2024-03-10 18:42:17 +0800

    rename to chat pilot.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ChatAgent
-Chat Agnet.
+# ChatPilot
+Chat Pilot.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 # Avoids IDE errors, but actual version is read from version.py
 __version__ = ""
-exec(open('chatagent/version.py').read())
+exec(open('chatpilot/version.py').read())
 
 if sys.version_info < (3,):
     sys.exit('Sorry, Python3 is required.')
@@ -14,18 +14,18 @@ with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name='chatagent',
+    name='chatpilot',
     version=__version__,
     description='Chat Agent toolkit.',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='XuMing',
     author_email='xuming624@qq.com',
-    url='https://github.com/shibing624/chatagent',
+    url='https://github.com/shibing624/chatpilot',
     license="Apache License 2.0",
     zip_safe=False,
     python_requires=">=3.10.0",
-    entry_points={"console_scripts": ["chatagent = chatagent.cli:main"]},
+    entry_points={"console_scripts": ["chatpilot = chatpilot.cli:main"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -46,6 +46,6 @@ setup(
         "pandas",
     ],
     packages=find_packages(exclude=['tests']),
-    package_dir={'chatagent': 'chatagent'},
-    package_data={'chatagent': ['*.*', 'data/*.txt']}
+    package_dir={'chatpilot': 'chatpilot'},
+    package_data={'chatpilot': ['*.*', 'data/*.txt']}
 )
