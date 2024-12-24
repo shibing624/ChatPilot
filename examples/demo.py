b7ef086f6a1c4008ee50643d9a84275457082197
shibing624 <shibing624@126.com>
2024-12-24 16:19:09 +0800

    update demo.

--- examples/demo.py
+++ examples/demo.py
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     )
     print(m)
     r = m.stream_run("一句话介绍北京")
-    print(r, "".join(r))
+    print(r)
+    for i in r:
+        print(i)
 
     # Enable search tool, url crawler tool, run python code tool
     m = AgenticaAgent(
@@ -28,11 +30,10 @@ if __name__ == '__main__':
         "俄乌战争的最新进展",
         "找出小于或等于90的所有素数。你可以实现古老的算法“埃拉托斯特尼筛法”，该算法通过多次筛选来找出一定范围内所有的素数。",
         "今天莫斯科有啥新闻消息，给出3个？",
-        "第一个新闻可信吗？给出理由。",
-        "https://arxiv.org/pdf/2302.13971.pdf 分析这个论文",
     ]
     for i in questions:
         print(i)
         r = m.stream_run(i)
-        print(r, "".join(r))
+        for i in r:
+            print(i.get_content_as_string())
         print("===")
